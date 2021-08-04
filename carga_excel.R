866b2e8e3470fe7000ae88159a086773728a9e83
Alejandro Sánchez Peralta <65984679+sanalexito@users.noreply.github.com>
2021-08-04 14:50:21 -0500

    Update carga_excel.R

--- carga_excel.R
+++ carga_excel.R
@@ -1,14 +1,14 @@
 
-# Se indica el nombre de la pestaña que se quiere cargar
+# Se indica el nombre de la pestaÃ±a que se quiere cargar
 hoja <- "2.1"
 
 # Es necesario indicar la ruta y nombre del archivo.
-ruta <- "D:/Varios/pagithub/proy2/II_Marco_Regulatorio_2016_est_nvo_pint.xlsx"
+ruta <- "./II_Marco_Regulatorio_2016_est_nvo_pint.xlsx"
 
 A <- openxlsx::read.xlsx(ruta, hoja, colNames = F, skipEmptyRows = F)
 
 # Casi todos los tabulados del INEGI comienzan con la leyenda "Estados Unidos Mexicanos" y 
-# dependiendo de la información contenida terminan con Zacatecas o con algún otro
+# dependiendo de la informaciÃ³n contenida terminan con Zacatecas o con algÃºn otro
 # texto. En este ejemplo se toma como paro la palabra "Nota:" que aparece en la nota 
 # acerca del pintado.
 
